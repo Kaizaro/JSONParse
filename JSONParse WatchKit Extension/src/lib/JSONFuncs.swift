38f57fc2860eae79e0965690fcf1a9d5eec14a07
Kaizaro <kaizaro78@gmail.com>
2021-10-10 15:26:24 +0300

    made all structs in JSON type as codalbe

---
+++ JSONParse WatchKit Extension/src/lib/JSONFuncs.swift
@@ -0,0 +1,21 @@
+//
+//  JSONFuncs.swift
+//  JSONParse WatchKit Extension
+//
+//  Created by Paul Zamshin on 10.10.2021.
+//
+
+import Foundation
+
+func readLocalJSONFile(forName name: String) -> Data? {
+    do {
+        if let filePath = Bundle.main.path(forResource: name, ofType: "json") {
+            let fileUrl = URL(fileURLWithPath: filePath)
+            let data = try Data(contentsOf: fileUrl)
+            return data
+        }
+    } catch {
+        print("error: \(error)")
+    }
+    return nil
+}
